61c8633b255d3cd9a9006815f85cd9477d46d3cc
Nicklaus <100449899+Nicklaus110@users.noreply.github.com>
2022-03-23 10:19:40 -0500

    Update Example.lua

--- deploy/Plugins/Example.lua
+++ deploy/Plugins/Example.lua
@@ -7,11 +7,11 @@
 -- Refer to our documentation to learn about plugins: https://nicklaus.gitbook.com/droplet/
 
 return function(Events)
-    Events.Pickup:Connect(function(Player, Product)
+    Events.Pickup:Connect(function(Player, Category, Product)
         print(Player.Name, ' picked up ', Product.Name)
     end)  -- This can also be used to display interface when a product goes out of stock - view our documentation!
 
-    Events.Restock:Connect(function(Player, Product)
+    Events.Restock:Connect(function(Player, Category, Product)
         print(Player.Name, ' restocked ', Product.Name)
     end) -- This can also be used to display interface when a product goes out of stock - view our documentation!
 
